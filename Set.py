29502fb1fc21f8c227057d356221836328a15b9e
mohamedhossam23 <88920277+mohamed-ma1703976@users.noreply.github.com>
2022-02-20 22:53:48 +0300

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,23 @@
+#set of strings 
+ss ={"python", "c++", "java"}
+
+#set of integers 
+si = {4,2,6,9}
+
+#set of boolean
+sb = {True,True,False,True}
+
+#mixed set
+s = {"python", "c++",5,4,3}
+
+
+#add 
+s.add("java")
+s.update(sb)
+
+
+
+#remove
+s.remove("java")
+s.pop()
+#s.clear()
