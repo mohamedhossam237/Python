73ea078d536097ea958d2cc106c556a52710114a
mohamedhossam23 <88920277+mohamed-ma1703976@users.noreply.github.com>
2022-03-04 12:25:34 +0300

    Update Tree.py

--- Data Structure  /Tree.py
+++ Data Structure  /Tree.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, letter):
+    def __init__(self, number):
         self.childleft = None
         self.childright = None
-        self.nodedata = letter
+        self.nodedata = number
 
 root = Node(8)
 root.childleft = Node(3)
@@ -13,3 +13,4 @@ root.childleft.childright.childright = Node(7)
 root.childleft.childright.childleft = Node(4)
 root.childright.childright = Node(14)
 root.childright.childright.childleft = Node(13)
+print(root.nodedata)
